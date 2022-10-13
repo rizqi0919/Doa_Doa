786b4ff0e5d267bee473bd91a873277fa7579704
rizqi0919 <115737352+rizqi0919@users.noreply.github.com>
2022-10-14 02:11:45 +0700

    Update doa.php

--- doa.php
+++ doa.php
@@ -7,11 +7,11 @@ $table = "<h3>Kumpulan Doa Sehari - Hari</h3>";
 for($i=0;$i<count($doa);$i++){
     $no=$i+1;
 $table .= "<table border=1>
-            <tr><ol>ID = {$doa[$i]->id}</ol>
+             <tr><ol>ID {$doa[$i]->id}</ol>
                 <ol>Doa = {$doa[$i]->doa}</ol>
                 <ol>Ayat = {$doa[$i]->ayat}</ol>
                 <ol>Latin = {$doa[$i]->latin}</ol>
-                <ol>Artinya = {$doa[$i]->artinya}</ol></tr>";
+                <ol>Artinya : {$doa[$i]->artinya}</ol></tr>";
     
 }
 $table .= "</table>";
